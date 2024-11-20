48da3ee9cba2d7408d0eb743855f8b722f12fa2b
Lucas Okido <lucascomunepennacchi@gmail.com>
2024-11-20 18:51:46 -0300

    User Login & Front end

---
+++ bluevelvet_music_store/src/main/java/com/api/bluevelvet_music_store/login/model/User.java
@@ -0,0 +1,61 @@
+package com.api.bluevelvet_music_store.login.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;  // Pode ser "admin" ou "user"
+
+    // Construtores, getters e setters
+    public User() {}
+
+    public User(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role=role;
+    }
+}
